c98270193236785b962a2d13f2eb83c5e3eae9e6
avocet-dev <82762900+avocet-dev@users.noreply.github.com>
2021-04-22 10:02:09 -0400

    Delete fileExplorer directory

--- fileExplorer/main.js
+++
@@ -1,8 +0,0 @@
-function myFunction(id) {
-    var x = document.getElementById(id);
-    if (x.className.indexOf("show") == -1) {
-      x.className += " show";
-    } else { 
-      x.className = x.className.replace(" show", "");
-    }
-}
